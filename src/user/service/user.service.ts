2fe132269d5108ad0dd34aa4de66c4f46139ad7e
annvrk01 <annvrk01@gmail.com>
2024-02-23 14:44:35 +0700

    update

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -9,11 +9,20 @@ import { UserResponse } from '../responses/user.response';
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private useReonsitory: Repository<User>,
-  ) {}
+    private userRepository: Repository<User>,
+  ) { }
 
   async findAll(): Promise<UserResponse[]> {
-    const users = await this.useReonsitory.find();
+    //     return await this.userRepository.findAndCount({
+    //       where: {
+    //           username: ILike(`%${keyword || ''}%`)
+    //       },
+    //       order: { id: 'DESC' }, // ORDER BY
+    //       take: 5, // Tương đương LIMIT
+    //       skip: 0, // Tương đương OFFSET
+    //   });
+    // }
+    const users = await this.userRepository.find();
     const usersRes = await Promise.all(
       users.map((user) => {
         let userRes = new UserResponse();
@@ -25,24 +34,29 @@ export class UserService {
   }
 
   findOne(id: number): Promise<User | null> {
-    return this.useReonsitory.findOneBy({ id });
+    const user = this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
   }
 
   async remove(id: number): Promise<void> {
-    await this.useReonsitory.delete({ id });
+    await this.userRepository.delete({ id });
   }
 
   async create(userDTO: UserDTO): Promise<UserResponse> {
     let user = new User();
     user.firstName = userDTO.firstName;
     user.email = userDTO.email;
-    const newUser = await this.useReonsitory.save(user);
+    const newUser = await this.userRepository.save(user);
     let userRes = new UserResponse();
     userRes.firstName = newUser.firstName;
     return userRes;
   }
 
   update(id: number, user: User): Promise<any | null> {
-    return this.useReonsitory.update({ id }, user);
+    return this.userRepository.update({ id }, user);
   }
+
 }
